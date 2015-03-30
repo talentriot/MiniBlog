08d6dbb3284a31f5f1cac1a0136894fb26547020
sergio alifano <sergio@gameplanlabs.com>
2015-03-30 04:39:22 -0600

    new container class

---
+++ Website/app_code/code/storageProviders/BlobPostStorage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GPL.AzureStorage;
+using Microsoft.WindowsAzure.Storage;
+
+/// <summary>
+/// Saves blog items to blob system storage
+/// </summary>
+public class BlobPostStorage : BlobStorageBase
+{
+    public BlobPostStorage(CloudStorageAccount cloudStorageAccount) : base(cloudStorageAccount)
+    {
+
+    }
+
+    public override string StorageContainerName
+    {
+        get { return "blogpost"; }
+    }
+
+
+}
